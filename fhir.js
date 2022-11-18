6a6b1db3b0bd76e82c0d6dd6073ad5aea3735be5
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-11-18 15:14:28 -0700

    added some style, less alerts and more output boxes, query for loaded systems

--- fhir.js
+++ fhir.js
@@ -1,16 +1,30 @@
 
 // needs headers, now CORS stuff too? Access-Control-Allow-Origin
-
-const  getFhir = async( theSystem, theCode) => {
     const myHeaders = new Headers({
             "Accept-Charset": "utf-8",
             "Content-Type": "application/fhir+json; charset=UTF-8",
             "User-Agent": "HAPI-FHIR/5.0.0 (FHIR Client; FHIR 4.0.1/R4; apache)"
     });
 
+not_implemented = function(x) {
+    alert(x + " is not implemented")
+}
+
+query_loaded_codesystems = async(theServer) => {
+    // http://20.119.216.32:8001/r4/CodeSystem?_summary=true
+    const request_string = theServer + "/CodeSystem?_summary=true" 
+    alert(request_string)
+    const response = await fetch(request_string, myHeaders)
+    const myJson = await response.json();
+    alert(JSON.stringify(myJson))
+
+}
+
+const  getFhir = async( theServer, theSystem, theCode) => {
 
     //const response = await fetch("http://20.119.216.32:8000/r4/CodeSystem/$lookup?system=http://loinc.org&code=LA6668-3", myHeaders)
-    const base_str = "http://20.119.216.32:8000/r4/CodeSystem/$lookup?"  // why the /$?
+    //const base_str = "http://20.119.216.32:8000/r4/CodeSystem/$lookup?"  // why the /$?
+    const base_str = theServer + "/CodeSystem/$lookup?"  // why the /$?
 
     //const system_str = "system=http://loinc.org"
     //const code_str = "&code=LA6668-3"
@@ -19,14 +33,26 @@ const  getFhir = async( theSystem, theCode) => {
     const code_str = "&code=" + theCode
     const requestString = base_str + system_str + code_str
 
-    alert(requestString)
+// alert(requestString)
 
     const response = await fetch(requestString, myHeaders)
     const myJson = await response.json();
-    alert(JSON.stringify(myJson))
+//  alert(JSON.stringify(myJson))
 
     // doesn't work the same for each ontology...
-    alert(myJson.parameter[1].valueString)
+    label = myJson.parameter[1].valueString
+
+    document.getElementById('output-system').value = theSystem
+    document.getElementById('output-code').value = theCode
+    document.getElementById('output-label').value = label
+
+    // **** good luck ***
+    // The structure isn't a simple map, but a pair of maps, one for name and the other for value.
+    //flat_parameter =  myJson.parameter.flat()
+    //alert(JSON.stringify(flat_parameter))
+    //alert(myJson.parameter['display']['valueString'])
+
+}
    /*
 
 LOINC for 66678-4
@@ -68,5 +94,3 @@ MONDO for CHEBI_33424
 
 
    */ 
-}
-
